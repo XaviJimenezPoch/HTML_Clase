2199b8c098295804c4723c8518834d8bf8ae4c80
XaviJimenezPoch <xavijimenezpoch98@gmail.com>
2026-04-01 12:19:26 +0200

    a

--- JS/Clase_2/script.js
+++ JS/Clase_2/script.js
@@ -1,7 +1,22 @@
-function saludar(nombre){
-    alert("Hola " + nombre);
+function saludar(nombre, edad) {
+  alert("Hola " + nombre + ", tu edad es " + edad);
 }
 
-saludar('Xavi'); 
-saludar('Marta');
-saludar('Pablo');
+saludar("Xavi", 27);
+saludar("Marta", 30);
+saludar("Pablo", 35);
+
+
+
+
+
+
+function calcularMayor(a, b, c) {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
+}
